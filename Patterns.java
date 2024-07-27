9ada994dd6d0c7429fe6e6e3d2a7a7c7e4b0d01d
Pratheekshakulal01 <pratheekshankulal@gmail.com>
2024-07-27 16:02:43 +0530

    commit2

--- Patterns.java
+++ Patterns.java
@@ -95,6 +95,35 @@ class Patterns{
             System.out.println();
         }
     }
+    void Pat10(){
+        System.out.println("\nPattern 10:");
+         int n=4;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+               System.out.print("*");
+                }
+                for(int k=1;k<=2*(n-i);k++){
+                    System.out.print(" ");
+                }
+                for(int l=1;l<=i;l++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for(int i=n;i>0;i--){
+                for(int j=1;j<=i;j++){
+                    System.out.print("*");
+                }
+                for(int k=1;k<=2*(n-i);k++){
+                    System.out.print(" ");
+                }
+                for(int l=1;l<=i;l++)
+                {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+    }
     public static void main(String args[]){
         Patterns P=new Patterns();
         P.Pat1();
@@ -106,5 +135,6 @@ class Patterns{
         P.Pat7();
         P.Pat8();
         P.Pat9();
+        P.Pat10();
     }
 }
